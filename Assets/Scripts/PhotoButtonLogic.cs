f154e933c1a0cfa6e099a92cfcda39458d6841a8
Arun Mikael Mehta <756271+arun02139@users.noreply.github.com>
2022-05-06 20:16:13 -0400

    Added Unity project

---
+++ Assets/Scripts/PhotoButtonLogic.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NativeCameraNamespace;
+
+public class PhotoButtonLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void StartCamera()
+    {
+        //NativeCameraNamespace.CameraCallback c;
+        //NativeCamera.TakePicture(CameraCallback callback, int maxSize = -1, PreferredCamera preferredCamera = PreferredCamera.Default)
+    }
+}
